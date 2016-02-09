981e2f94751898119d1e1ae7e439c49519e3f66a
dccharacter <yrr.yrr@gmail.com>
2016-02-09 18:53:30 -0500

    Added board_ArduinoNano-doubeLaser.h to tracking

---
+++ RF3Sens/board_ArduinoNano_doubleLaser.h
@@ -0,0 +1,55 @@
+//###########################################################################################
+// RF3Sens (RoboForum triangulation range sensor)
+// http://roboforum.ru/forum107/topic15929.html (official language for this forum - russian)
+// MIT License
+// 2015-2016
+// Дмитрий Лилик (aka Dmitry__ @ RoboForum.ru)
+// Андрей Пожогин (aka dccharacter  @ RoboForum.ru)
+// Сергей Тараненко (aka setar @ RoboForum.ru)
+//###########################################################################################
+
+/* board_ArduinoNano.h */
+
+#if defined(use_nCS)
+  //pin_nCS  PB2 SS D10
+  #define nCS_PIN        2
+  #define nCS_DDR        DDRB
+  #define nCS_PORT       PORTB
+#endif 
+
+//pin_led
+//PB5 D13
+#define led_PIN         5
+#define led_DDR         DDRB
+#define led_PORT        PORTB
+
+//pin_nClock (SCK)
+//PB4 D12
+#define nClock_PIN      4
+#define nClock_DDR      DDRB
+#define nClock_PORT     PORTB
+
+//MOSI
+//PB3  D11
+#define SDIO_PIN        3
+#define SDIO_DDR        DDRB
+#define SDIO_PORT       PORTB
+#define SDIO_IN    PINB
+
+//PD4 TRIGGER_PIN
+//PD4 D4
+#define TRIG_PIN        4
+#define TRIG_DDR        DDRD
+#define TRIG_IN    PIND
+
+//Laser 1 VCC
+//PD5 D5
+#define laser1_vcc_PIN   5
+#define laser1_vcc_DDR   DDRD
+#define laser1_vcc_PORT  PORTD
+
+//Laser 2 VCC
+//PD6 D6
+#define laser2_vcc_PIN   6
+#define laser2_vcc_DDR   DDRD
+#define laser2_vcc_PORT  PORTD
